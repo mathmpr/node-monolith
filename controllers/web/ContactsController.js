ea85fdfd9f4abbefe1cf8e1ab7e72d58c3734971
mathmpr <matheusprador@gmail.com>
2025-07-29 23:51:10 -0300

    more progress with contacts

--- controllers/web/ContactsController.js
+++ controllers/web/ContactsController.js
@@ -1,5 +1,19 @@
 module.exports = {
     index: async (req, res) => {
-        res.render('contacts/index');
+
+        const contacts = [
+            {
+                name: 'João Silva',
+                phone: '11 91234-5678',
+                email: 'joao@email.com',
+                address: 'Rua das Flores, 123',
+                photo: '/images/joao.jpg',
+            }
+        ];
+
+        res.render('contacts/index', {
+            user: { name: 'Matheus' },
+            contacts,
+        });
     },
 };
